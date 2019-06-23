b42d841478ea35446ff4b765b522eea5118c98ba
manjurulhoque <rumimanzurulhoque@gmail.com>
2019-06-24 00:10:48 +0600

    Worked on django superadmin

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from .views import *
+
+admin.site.register(About)
+admin.site.register(Service)
+admin.site.register(RecentWork)
+admin.site.register(Client)
--- portfolio/urls.py
+++ portfolio/urls.py
@@ -1,7 +1,9 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('core.urls')),  # include our custom app urls here
-]
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
